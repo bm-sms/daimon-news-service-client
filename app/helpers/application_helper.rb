7a218d968f7167e9c60c2998cd812424bfdabecf
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-10-28 16:30:15 +0900

    Add sample to customize view using helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_meta_tags(title: nil)
+    title ||= 'Customized Press'
+
+    content_for :meta_tags do
+      render 'meta_tags', title: title
+    end
+  end
 end
