e8f35ff26ba14383e7c3f4c616f5814965926507
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-10-28 11:14:04 +0900

    Simplify

--- news_service/lib/generators/news_service/views_generator.rb
+++ news_service/lib/generators/news_service/views_generator.rb
@@ -1,19 +1,8 @@
-class NewsService::ViewsGenerator < Rails::Generators::NamedBase
+class NewsService::ViewsGenerator < Rails::Generators::Base
   source_root File.expand_path('../../../../app/views/news_service', __FILE__)
 
-  desc 'Copy views from news_service to application'
+  desc 'Copies news_service views to your application'
   def copy_views
-    case file_name
-    when 'posts'
-      copy_posts_views
-    else
-      raise 'Unsupported generator.'
-    end
-  end
-
-  private
-
-  def copy_posts_views
     filename_pattern = File.join(self.class.source_root, 'posts', '*.html.erb')
 
     Dir.glob(filename_pattern).map {|f| File.join('posts', File.basename(f)) }.each do |f|
