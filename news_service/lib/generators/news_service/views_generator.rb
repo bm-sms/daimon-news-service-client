d6d49c06a62c5bab7b15868978078fb9694d35bf
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-10-28 16:37:56 +0900

    Support views generator to filter pattern

--- news_service/lib/generators/news_service/views_generator.rb
+++ news_service/lib/generators/news_service/views_generator.rb
@@ -1,11 +1,23 @@
-class NewsService::ViewsGenerator < Rails::Generators::Base
+class NewsService::ViewsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../../../../app/views/news_service', __FILE__)
 
   desc 'Copies news_service views to your application'
   def copy_views
-    filename_pattern = File.join(self.class.source_root, 'posts', '*.html.erb')
+    filename_pattern =
+      case file_name
+      when 'all'
+        '*.html.erb'
+      when 'partial'
+        '_*.html.erb'
+      when 'layout'
+        '*.html.erb' # TODO Ignore partial
+      else
+        raise "Unsupported type: `#{file_name}`. Please use the one of candidates(partial, layout or all)"
+      end
 
-    Dir.glob(filename_pattern).map {|f| File.join('posts', File.basename(f)) }.each do |f|
+    filepath_pattern = File.join(self.class.source_root, 'posts', filename_pattern)
+
+    Dir.glob(filepath_pattern).map {|f| File.join('posts', File.basename(f)) }.each do |f|
       copy_file f, "app/views/news_service/#{f}"
     end
   end
