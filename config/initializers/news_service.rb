ebe43fc739fee0dd0de81b0bfd0ebf4b4f4a0414
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-11-04 17:09:21 +0900

    Re-enable application layout

--- config/initializers/news_service.rb
+++ config/initializers/news_service.rb
@@ -3,5 +3,5 @@ NewsService.configure do |config|
   config.service_url = ENV.fetch('NEWS_SERVICE_URL', 'http://localhost:3000')
   config.site_token  = ENV.fetch('NEWS_SERVICE_TOKEN', '1')
 
-  # config.detect_layout_proc = ->(controller) { 'application' }
+  config.detect_layout_proc = ->(controller) { 'application' }
 end
