483ff987de66aff18aaf264956f67e300691d984
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-11-04 16:00:27 +0900

    Add view generator

---
+++ news_service/lib/generators/news_service/views_generator.rb
@@ -0,0 +1,12 @@
+class NewsService::ViewsGenerator < Rails::Generators::Base
+  source_root File.expand_path('../../../../app/views/news_service', __FILE__)
+
+  desc 'Copies news_service views to your application'
+  def copy_views
+    filepath_pattern = File.join(self.class.source_root, 'posts', '*.html.erb')
+
+    Dir.glob(filepath_pattern).map {|f| File.join('posts', File.basename(f)) }.each do |f|
+      copy_file f, "app/views/news_service/#{f}"
+    end
+  end
+end
